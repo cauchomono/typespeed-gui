b4a88222b51e9af18ef521925f9b572c08057496
Sebastian <sebasfo06@hotmail.com>
2021-10-28 17:08:47 -0500

    reference methods and attributes

---
+++ typetest.py
@@ -0,0 +1,29 @@
+class TypeTest:
+
+    def __init__(self,text):
+        self.TEXT = text
+
+    def game_timer(self):
+        pass
+
+    def word_generator(self):
+        pass
+
+    def word_counter(self):
+        pass
+
+    def char_counter(self):
+        pass
+
+    def letter_counter(self):
+        pass
+
+    def score(self):
+        pass
+
+    def reset_scores(self):
+        pass
+
+    def words_judge(self):
+        pass
+
